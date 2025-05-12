15be572220a54c18cbb9e36cd2ad58634611fec8
Master-Infrit <matheus.sam.pires@gmail.com>
2025-05-12 01:51:33 +0000

    adicionei comparacao de cartas para cada uma variavel

--- supertrunfo.c
+++ supertrunfo.c
@@ -19,7 +19,6 @@ int main() {
     int pontos_turisticos2;
     float desidade_populacional2;
     float pib_per_capita2;
-
     
 
     // Atribuindo valores às variáveis da Carta 1
@@ -73,12 +72,56 @@ int main() {
     float super_poder1 = populacao + area + pib + pontos_turisticos + (desidade_populacional / pib_per_capita);
     float super_poder2 = populacao2 + area2 + pib2 + pontos_turisticos2 + (desidade_populacional2 / pib_per_capita2);
 
+    printf("\nSuper Poder da Carta 1: %.2f\n", super_poder1);
+    printf("Super Poder da Carta 2: %.2f\n", super_poder2);
+    // Comparando os super poderes
+
     if (super_poder1 > super_poder2) {
-        printf("A carta 1 é mais forte!\n");
+        printf("Super poder: A carta 1 venceu!\n");
     } else if (super_poder1 < super_poder2) {
-        printf("A carta 2 é mais forte!\n");
+        printf("Super poder: A carta 2 venceu!\n");
     } else {
         printf("As cartas são iguais!\n");
+    }
+
+
+    if (populacao2 > populacao) {
+        printf("Populacao: A carta 2 venceu!\n");
+    } else if (populacao2 < populacao) {
+        printf("Populacao: A carta 1 venceu!\n");
+    } else {
+        printf("As cartas têm a mesma população!\n");
+    }
+
+    if (area2 > area) {
+        printf("Area: A carta 2 venceu!\n");
+    } else if (area2 < area) {
+        printf("Area: A carta 1 venceu!\n");
+    } else {
+        printf("As cartas têm a mesma área!\n");
+    }
+
+    if (pib2 > pib) {
+        printf("Pib: A carta 2 venceu!\n");
+    } else if (pib2 < pib) {
+        printf("Pib: A carta 1 venceu!\n");
+    } else {
+        printf("As cartas têm o mesmo PIB!\n");
+    }
+    if (pontos_turisticos2 > pontos_turisticos) {
+        printf("Pontos Turisticos: A carta 2 venceu!\n");
+    } else if (pontos_turisticos2 < pontos_turisticos) {
+        printf("Pontos Turisticos: A carta 1 venceu!\n");
+    } else {
+        printf("As cartas têm o mesmo número de pontos turísticos!\n");
+    }
+
+    if (desidade_populacional2 < desidade_populacional) {
+        printf("Densidade Populacional: A carta 2 vence!\n");
+    } else if (desidade_populacional2 > desidade_populacional) {
+        printf("Densidade Populacional: A carta 1 perde!\n");
+    } else {
+        printf("As cartas têm a mesma densidade populacional!\n");
     }
 
     return 0;
