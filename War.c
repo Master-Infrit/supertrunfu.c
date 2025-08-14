07229bd00c5507947f14658749d2d0fcb054c583
Master-Infrit <matheus.sam.pires@gmail.com>
2025-08-14 02:54:46 +0000

    definindo as variaveis

--- War.c
+++ War.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX[5]
+#define
+
+struct War {
+   char Territorio [50];
+   char Cor [10];
+   int tropas;
+}
+
+int main(){
+  struct War[MAX];
+  int territorios = 0;
+  int opcao;
+}
+do {
+    // ============== Exibição do Menu Do Jogo WAR ================
+    printf{"======WAR======\n\n"};
+    printf{"Jogar\n"};
+    printf{"opções\n"};
+    printf{"Sair\n\n"}
+    printf{"Escolha uma opção: "};
+
+    scanf{"%d", &opcao};
+
+    switch (opcao) {
+        case 1:
+          printf("Cadastro de Territorio\n\n");
+
+          if (territorios < MAX) {
+            printf("Territorio: ");
+            fgets(War[MAX].territorio, 
+
+          }
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
 
